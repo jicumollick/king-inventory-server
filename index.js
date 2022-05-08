12ecf0b0b52653db991e8d02bdfd00f306aaf9c9
jicu mollick <jikumollick82@gmail.com>
2022-05-08 19:46:05 +0600

    environment variable setup done

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
 
@@ -10,8 +13,7 @@ app.use(express.json());
 // DB_USER = dbuser1;
 // DB_PASS = m8Dj1cBhKL4codyK;
 
-const uri =
-  "mongodb+srv://dbuser1:m8Dj1cBhKL4codyK@cluster0.le9xm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.le9xm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
